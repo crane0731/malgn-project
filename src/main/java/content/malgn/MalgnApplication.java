b5fa0c1a3f27b1228728f43dcbedb00addb2d728
crane0731 <dlwnsgkr8318@gmail.com>
2026-03-08 17:17:25 +0900

    프로젝트 기본 세팅 완료

---
+++ src/main/java/content/malgn/MalgnApplication.java
@@ -0,0 +1,13 @@
+package content.malgn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalgnApplication.class, args);
+	}
+
+}
